7f133d910d299f63e9e5ebba976055265f301f8b
José Santisteban <orlando11virgo@gmail.com>
2026-09-14 12:51:49 -0500

    haber con compras

--- compras.php
+++ compras.php
@@ -302,7 +302,7 @@ include("header.php");
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/js/tom-select.complete.min.js"></script>
-<script src="js/device-tracking.js"></script>
+<script src="assets/js/device-tracking.js"></script>
 <script>
 const CONTROLADOR_COMPRAS  = 'controllers/clssCompra.php';
 const CONTROLADOR_PROVEEDORES = 'controllers/clssProveedor.php';
--- materiales.php
+++ materiales.php
@@ -123,6 +123,7 @@ include("header.php");
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="assets/js/device-tracking.js"></script>
 <script>
 const CONTROLADOR_MATERIALES = 'controllers/clssMaterial.php';
 const CONTROLADOR_UNIDADES   = 'controllers/clssUnidadMedida.php';
