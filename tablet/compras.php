6bcea055cd63b136dcf7888d782f566bbb2c2463
José Santisteban <135945021+SantistebanJose@users.noreply.github.com>
2026-09-22 13:08:41 -0500

    a la mrd

--- tablet/compras.php
+++ tablet/compras.php
@@ -384,7 +384,7 @@ $operarioNombre = $_SESSION['operario_nombre'] ?? 'Operario';
 const OPERARIO_ID     = <?= json_encode($operarioId) ?>;
 const OPERARIO_NOMBRE = <?= json_encode($operarioNombre) ?>;
 
-const CONTROLADOR_COMPRA = '../controllers_tablet/clssCompraTablet.php';
+const CONTROLADOR_COMPRA = '../controllers_tablet/clssCompraConductor.php';
 const modalCompra = new bootstrap.Modal(document.getElementById('modalCompra'));
 
 // llamar() es el helper compartido (ver app-common.js) que hace un POST
